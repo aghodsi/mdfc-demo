ccacad2c5efa31bd36a3450d8279c2e8b0f38f19
Sebastiaan Drinkenburg <sdrinkenburg@microsoft.com>
2022-11-20 23:05:18 +0100

    mycode-example

---
+++ mycode-example.py
@@ -0,0 +1,10 @@
+import python
+
+TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
+TWITTER_OAUTH_SECRET = "dkejkdjekdjkejdkjekdjekjdkjed"
+
+AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
+
+from Function f
+where f.getScope() instanceof Function
+select f
